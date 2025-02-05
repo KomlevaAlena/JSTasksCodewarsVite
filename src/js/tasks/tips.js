6a56db650cebfcd544904abeb441a70a3bcf3ce1
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-02-05 22:29:53 +0500

    решение прошло тест

--- src/js/tasks/tips.js
+++ src/js/tasks/tips.js
@@ -10,16 +10,30 @@
 // Оценка нечувствительна к регистру (поэтому "отлично" = "ОТЛИЧНО"). Если получена нераспознанная оценка, вам необходимо вернуть:
 
 // "Оценка не распознана" в Javascript
+// function calculateTip(amount, rating) {
+//     let percent = {
+//         tarrible: 0,
+//         poor: amount * 5 / 100,
+//         good: amount * 10 / 100,
+//         great: amount * 15 / 100,
+//         excellent: amount * 20 / 100
+//     }
+//     return Math.ceil(percent[rating.toLowerCase()] ?? 'Rating not recognised');
+//   };
+//непроходит тест при нуле и результате отлисном от представленных оценок
+
 function calculateTip(amount, rating) {
-    let percent = {
-        tarrible: 0,
-        poor: amount * 5 / 100,
-        good: amount * 10 / 100,
-        great: amount * 15 / 100,
-        excellent: amount * 20 / 100
-    }
-    return Math.ceil(percent[rating.toLowerCase()] ?? 'Rating not recognised');
-  };
+    const tips = {
+          'Excellent': 0.2,
+          'Great': 0.15,
+          'Good': 0.1,
+          'Poor': 0.05,
+          'Terrible': 0
+      };
+      rating = rating.charAt(0).toUpperCase() + rating.slice(1).toLowerCase();
+      
+      return tips.hasOwnProperty(rating) ? Math.ceil(amount * tips[rating]) : 'Rating not recognised';
+  }
 
   //calculateTip(300, 'Good');
   console.log(calculateTip(300, 'Good'));
