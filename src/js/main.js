e5964560923be0d27367c805c061edf780da3f86
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-02-06 20:46:01 +0500

    add mergeArrays

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import '../scss/style.scss';
 import {calculateTip} from './tasks/tips';
 import {squareSum} from './tasks/Square(n)Sum';
+import {mergeArrays} from './tasks/mergeTwoArrays';
 
 console.log(calculateTip(300, 'Good'));
 console.log(squareSum([1, 2, 3]));
+console.log(mergeArrays([1, 2, 3], ['a', 'b', 'c']));
