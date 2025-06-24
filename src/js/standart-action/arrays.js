bf020b5d58dc4c577ccf8f124af26172ce7f1b10
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-06-25 01:06:10 +0500

    add p

--- src/js/standart-action/arrays.js
+++ src/js/standart-action/arrays.js
@@ -5,6 +5,13 @@ export const createArray = () => {
     // let numbers = new Array(1, 2, 3);
     simpleArray.push('еще объект в конце массива');
     simpleArray.unshift('объект в начале массива'); //добавили объект в начало массива
+    simpleArray.push(6, 5, 4, 3, 2, 1, 0);//добавили объект в конец массива
+    simpleArray.pop();//удалили последний элемент
+    simpleArray.shift();//удалили первый элемент
 
+    simpleArray.forEach(item => console.log(item));// forEach (проход без возврата)
+     
+    const doubled = simpleArray.map(num => num * 2); // map (трансформация)
+    console.log(doubled);
     console.log(simpleArray);
 };
