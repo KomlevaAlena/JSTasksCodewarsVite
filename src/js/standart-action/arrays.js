4ad88041d1c89edab23f2e0eef86d0275b8570f1
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-06-25 22:11:27 +0500

    arrays copy merged

--- src/js/standart-action/arrays.js
+++ src/js/standart-action/arrays.js
@@ -1,17 +1,41 @@
 export const createArray = () => {
     const simpleArray = []; //создали массив
-    simpleArray.push('объект в конце массива'); //добавили объект в конец массива
+    simpleArray.push(25); //добавили объект в конец массива
     // 2. Конструктор Array (используется реже)
-    // let numbers = new Array(1, 2, 3);
-    simpleArray.push('еще объект в конце массива');
-    simpleArray.unshift('объект в начале массива'); //добавили объект в начало массива
+    let twoArray = new Array(1, 2, 3);
+
+    simpleArray.push(15);
+    simpleArray.unshift(31); //добавили объект в начало массива
     simpleArray.push(6, 5, 4, 3, 2, 1, 0);//добавили объект в конец массива
     simpleArray.pop();//удалили последний элемент
     simpleArray.shift();//удалили первый элемент
 
-    simpleArray.forEach(item => console.log(item));// forEach (проход без возврата)
+    simpleArray.forEach(item => console.log(item));// forEach (проход без возврата, не возвращает новый массив) 
      
-    const doubled = simpleArray.map(num => num * 2); // map (трансформация)
-    console.log(doubled);
+    const doubled = simpleArray.map(num => num * 2); // map (трансформация, возвращает новый массив)
+
+    const evens = simpleArray.filter(num => num % 2 === 0); // filter (фильтрация, вернет массив из четных элементов)
+
+    const sum = simpleArray.reduce((acc, num) => acc + num, 0); // reduce (сумма всех элементов массива, acc (accumulator)который хранит промежуточный результат, num (currentValue) — текущий элемент массива)
+
+    
+    const found = simpleArray.find(num => num > 1); // find (первый подходящий)
+
+    const includesNumber = simpleArray.includes(2); // includes (проверка наличия элемента с таким значением true-falce)
+
+    const someNumber = simpleArray.some(num => num > 2); // some (хотя бы один подходящий элемент)
+
+    const everyNumber = simpleArray.every(num => num < 5); // every (подходят ли все элементы)
+
+    const copy = [...simpleArray]; //(копирование массива)
+    const sliceCopy = simpleArray.slice();//(копирование массива)
+
+    const merged = [...simpleArray, ...twoArray]; // (объединение массивов)
+
+
+    // console.log(doubled);
+    // console.log(evens);
     console.log(simpleArray);
+    console.log(twoArray);
+    console.log(merged);
 };
